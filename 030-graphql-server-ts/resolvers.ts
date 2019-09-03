ce33d40d215e4c2a5404ea1909619b0c0cdd42e2
Christian Schwendtner <22419197+cschwendtner@users.noreply.github.com>
2019-09-03 07:42:56 +0200

    Initial commit

---
+++ 030-graphql-server-ts/resolvers.ts
@@ -0,0 +1,18 @@
+// import { QueryResolvers, BookResolvers } from './graphql';
+
+// export const Query: QueryResolvers = {
+//   theBook: (root, args, context) =>  {
+//     // 
+//   }
+// };
+
+// export const Book: BookResolvers = {
+//     author: (book, args, context) => { 
+//         // 
+//     }
+// };
+
+// export const resolvers = {
+//     Query,
+//     Book
+// };
